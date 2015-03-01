b004eae609ae1c8a60f2540083e2a6146d5a4483
JonLachen <jontxu_4_@hotmail.com>
2015-03-01 22:35:28 +0100

    Trabajo de la semana
    Se consigue hacer funcionar los templates de backbone.js aunque aun no
    se domina su comportamiento del todo. Se puede consultar la información
    del usuario de forma correcta.

---
+++ Babelium project HTML5/backbone/vistas/UserInfo.js
@@ -0,0 +1,21 @@
+var UserView = Backbone.View.extend({
+	tagname:'li',
+	my_template: _.template("<br>"
+    +"<p>User name: <%= userName %></p><br>"
+    +"<p>Email: <%= email %></p><br>"
+	+"<p>Real name: <%= realName %></p><br>"
+	+"<p>Real last name: <%= realLastname %></p><br>"
+	+"<p>Mother tongue: <%= motherLang %></p><br>"
+	+"<p>Other tongue: <%= otherLang %></p>"
+	+"<p>Level: <%= oLLevel %></p><br>"
+	+"<p>Interested in: <%= interestedIn %></p>"
+	+"<p>Level: <%= iILevel %></p>"),
+
+	initialize: function(){
+		this.render();
+	},
+
+	render: function(){
+		this.$el.html(this.my_template(this.model.toJSON()));
+	}
+})
--- Babelium project HTML5/js/codigo.js
+++ Babelium project HTML5/js/codigo.js
@@ -1,11 +1,15 @@
 window.onload = function()
 {
+	var currentUser; //Para almacenar un User de backbone con toda la información del JSON obtenido de SQL
 	var bLogin = document.getElementById("botonaceptar");
 	var bRegis = document.getElementById("botonRegistrar");
 
-	var prueba = new User();
-	var userNav = new UserNav({model:prueba});
-	$("#useroff").html(userNav.el);
+
+//CONSIDERAR CREAR UNA FUNCION INITIALIZE PARA ESTE TIPO DE COSAS
+	var currentUserNav = new UserNavOff();
+	$("#userNav").html(currentUserNav.el);
+
+
 
 	bLogin.onclick = function(){
 		var user = document.getElementById("user").value;
@@ -13,9 +17,12 @@ window.onload = function()
 
 		if(user == "jon" && pass == "lachen")
 		{
-			ocultar("useroff");
-			mostrar("useron");
-			document.getElementById("username").innerHTML=user;
+			currentUser = new User();
+			currentUser.set({userName: "Jon", email: "asdf@gmail.com", realName: "Jon", realLastname: "Lachén", motherLang: "Spanish", otherLang: "English", oLLevel: "A1", interestIn: "German", iILevel: "B1"});
+			userNavSwitch();
+			//ocultar("useroff");
+			//mostrar("useron");
+			//document.getElementById("username").innerHTML=user;
 			ocultar("modalLogin");
 
 			//Falta tema de recordar, habría que usar localstorage. También recuperar pass falta.
@@ -32,19 +39,37 @@ window.onload = function()
 		var member = new User();
 		member.set({userName: document.getElementById("userReg").value, email: document.getElementById("emailReg").value, password: document.getElementById("passwordReg").value,	realName: document.getElementById("nReal").value, realLastname: document.getElementById("aReal").value, motherLang: document.getElementById("mLang").value, otherLang: document.getElementById("oLang").value, oLLevel: document.getElementById("oLangLvl").value, interestIn: document.getElementById("iLang").value, iILevel: document.getElementById("iLangLvl").value});
 		document.getElementById("register").reset();
-		var userView = new UserView({model:member});
-		$("#userinfo").html(userView.el);
+	}
+
+	function userNavSwitch()
+	{
+		currentUserNav.unbind();
+		if(currentUserNav instanceof UserNavOff)
+		{
+			currentUserNav = new UserNavOn(currentUser);
+			$("#userNav").html(currentUserNav.el);
+		}
+		else
+		{
+			currentUserNav = new UserNavOff();
+			$("#userNav").html(currentUserNav.el);
+		}
 	}
 
 	function mostrar(id)
 	{
 		var el = document.getElementById(id);
-		el.style.visibility = "visible";
+		el.style.display = "block";
 	}
 	function ocultar(id)
 	{
 		var el = document.getElementById(id);
-		el.style.visibility = "hidden";
+		el.style.display = "none";
 	}
 
+	function showUserInfo()
+	{
+		var userView = new UserView({model:currentUser});
+		$("#mainBody").html(userView.el);
+	}
 }
