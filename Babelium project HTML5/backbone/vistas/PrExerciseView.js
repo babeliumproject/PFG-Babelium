7a97c7088e7b8662212b20df13cd0b868eeac8f9
Jon Lachén Leal <jontxu_4_@hotmail.com>
2015-05-08 13:28:15 +0200

    Ajuste
    Pequeño ajuste en la visibilidad del formulario upload y añadido tag de
    video por aportar visualización de como quedaría

--- Babelium project HTML5/backbone/vistas/PrExerciseView.js
+++ Babelium project HTML5/backbone/vistas/PrExerciseView.js
@@ -25,7 +25,12 @@ var PrExercise = Backbone.View.extend({
     },
     render: function ()
     {
-        this.$el.html(this.my_template());
+        this.$el.html("<video width='320' height='240' controls>"
+        +"<source src='movie.mp4' type='video/mp4'>"
+        +"<source src='movie.ogg' type='video/ogg'>"
+        +"Your browser does not support the video tag."
+        +"</video>");
+        this.$el.append(this.my_template());
     },
     
     record: function ()
