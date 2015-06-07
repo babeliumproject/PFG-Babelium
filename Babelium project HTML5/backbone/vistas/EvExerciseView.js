e499b88370f9de29e61fc9900b18141e3db837ee
Jon Lachén Leal <jontxu_4_@hotmail.com>
2015-06-07 13:39:43 +0200

    Videos 0.9.1
    Funcionan los videos de respuesta ahora pero hay que ajustar el
    reproductor de video que sale en otra posicion y tamaño :dancer:  closes
    #6

--- Babelium project HTML5/backbone/vistas/EvExerciseView.js
+++ Babelium project HTML5/backbone/vistas/EvExerciseView.js
@@ -4,7 +4,7 @@ var EvExercise = Backbone.View.extend({
         +"<p><h2 id='babelium-exercise-title'></h2></p>"
         +"<article>"
         +"<div class='no-overflow'>"
-        +"<object type='application/x-shockwave-flash' id='babeliumPlayer' name='babeliumPlayer' align='middle' data='http://babeliumproject.com/babeliumPlayer.swf' width='640' height='380' style='height: 332px; width: 500px;'>"
+        +"<object type='application/x-shockwave-flash' id='babeliumPlayer' name='babeliumPlayer' align='middle' data='http://babeliumproject.com/babeliumPlayer.swf' width='640' height='380' style='height: 400px; width: 900px;'>"
         +"<param name='quality' value='high'>"
         +"<param name='bgcolor' value='#000000'>"
         +"<param name='allowscriptaccess' value='always'>"
@@ -14,12 +14,26 @@ var EvExercise = Backbone.View.extend({
         +"</object>"
         +"</div>"
         +"</article>"
-        +"<article id='exerciseInfo' class='exerciseInfo aligned'>"
+        +"<article id='exerciseInfo' class='evaluation aligned'>"
+        +"<p><h2>Rate the user's response</h2></p>"
+        +"<p><h4>Compulsory evaluation data:</h4></p>"
+        +"<label>Intonation & accent</label><select><option value='1'>1</option><option value='2'>2</option><option value='3'>3</option><option value='4'>4</option><option value='5'>5</option></select><br>"
+        +"<label>Pronunciation</label><select><option value='1'>1</option><option value='2'>2</option><option value='3'>3</option><option value='4'>4</option><option value='5'>5</option></select><br>"
+        +"<label>Rhythm</label><select><option value='1'>1</option><option value='2'>2</option><option value='3'>3</option><option value='4'>4</option><option value='5'>5</option></select><br>"
+        +"<label>Spontaneity</label><select><option value='1'>1</option><option value='2'>2</option><option value='3'>3</option><option value='4'>4</option><option value='5'>5</option></select><br>"
+        +"<label>Overall Score</label><select><option value='1'>1</option><option value='2'>2</option><option value='3'>3</option><option value='4'>4</option><option value='5'>5</option></select><br>"
+        +"<input id='txtComCheck' type='checkbox'><label>Include a text comment</label><br>"
+        +"<div id='txtComDiv' style='display:none'><textarea id='txtCom'></textarea></div>"
+        +"<input id='vidComCheck' type='checkbox'><label>Include a video comment</label><br>"
+        +"<div id='vidComDiv' style='display:none'>Aquí va el video</div>"
+        +"<div style='text-align:center, padding-left:10px' id='evButtons'><button id='sendEv'>Send evaluation</button><button id='reset'>Reset evaluation</button>"
+        +"</article>"
         +"</section>"
     ),
     events:
             {
-
+                'change #txtComCheck' : 'addTxt',
+                'change #vidComCheck' : 'addVid'
             },
     initialize: function (options)
     {
@@ -40,6 +54,8 @@ var EvExercise = Backbone.View.extend({
             alert(err.Message);
         });
 
+//init("babeliumproject.com", "es", "1", {"responseName":"resp-1347271137704","responseRole":"Shaman","subtitleId":"140","responseThumbnailUri":"default.jpg","exerciseId":"73","exerciseName":"U1MbBtkIGZQ","duration":"32","exerciseThumbnailUri":"default.jpg","title":"Sintel"}, [{"id":"1293","showTime":"1","hideTime":"6","text":"So, what brings you to the land of the gatekeepers?","exerciseRoleId":"265","exerciseRoleName":"Shaman","subtitleId":"140"},{"id":"1294","showTime":"7","hideTime":"10","text":"I'm searching for someone.","exerciseRoleId":"266","exerciseRoleName":"Sintel","subtitleId":"140"},{"id":"1295","showTime":"10.5","hideTime":"15.5","text":"Someone very dear? A kindred spirit?","exerciseRoleId":"265","exerciseRoleName":"Shaman","subtitleId":"140"},{"id":"1296","showTime":"17.05","hideTime":"18.37","text":"A dragon.","exerciseRoleId":"266","exerciseRoleName":"Sintel","subtitleId":"140"},{"id":"1297","showTime":"21","hideTime":"25.6","text":"A dangerous quest for a lone hunter.","exerciseRoleId":"265","exerciseRoleName":"Shaman","subtitleId":"140"},{"id":"1298","showTime":"27.24","hideTime":"30","text":"I've been alone for as long as I can remember.","exerciseRoleId":"266","exerciseRoleName":"Sintel","subtitleId":"140"}], "", "");
+                      
         var ctx = this;
         // Hay que hacer esperar un poco para obtener las respuestas a todas las llamadas necesarias
         var varCheck = setInterval(function()
@@ -83,12 +99,14 @@ var EvExercise = Backbone.View.extend({
                 ctx.render(exData,respData,exSubs);
             }
         },500);
-        //¿Y que hago yo con esto? 
-        //bpPlayer.exerciseSource(this.options.exid:String):Void
     },
     render: function (exData,respData,exSubs)
     {
-        init("babeliumproject.com", "en", "1", respData, exSubs, "", "");
+        var exercise = {'exerciseId':exData.response.id,'exerciseName':exData.response.name,'duration':exData.response.duration,'exerciseThumbnailUri':exData.response.thumbnailUri,'title':exData.response.title};
+console.log(respData);
+        console.log("jlachen,en,1,"+ JSON.stringify(respData)+","+ JSON.stringify(exSubs)+",'',''");
+        init("babeliumproject.com", "es", "1", {"responseName":"resp-1347271137704","responseRole":"Shaman","subtitleId":"140","responseThumbnailUri":"default.jpg","exerciseId":"73","exerciseName":"U1MbBtkIGZQ","duration":"32","exerciseThumbnailUri":"default.jpg","title":"Sintel"}, [{"id":"1293","showTime":"1","hideTime":"6","text":"So, what brings you to the land of the gatekeepers?","exerciseRoleId":"265","exerciseRoleName":"Shaman","subtitleId":"140"},{"id":"1294","showTime":"7","hideTime":"10","text":"I'm searching for someone.","exerciseRoleId":"266","exerciseRoleName":"Sintel","subtitleId":"140"},{"id":"1295","showTime":"10.5","hideTime":"15.5","text":"Someone very dear? A kindred spirit?","exerciseRoleId":"265","exerciseRoleName":"Shaman","subtitleId":"140"},{"id":"1296","showTime":"17.05","hideTime":"18.37","text":"A dragon.","exerciseRoleId":"266","exerciseRoleName":"Sintel","subtitleId":"140"},{"id":"1297","showTime":"21","hideTime":"25.6","text":"A dangerous quest for a lone hunter.","exerciseRoleId":"265","exerciseRoleName":"Shaman","subtitleId":"140"},{"id":"1298","showTime":"27.24","hideTime":"30","text":"I've been alone for as long as I can remember.","exerciseRoleId":"266","exerciseRoleName":"Sintel","subtitleId":"140"}], "", "");
+
         this.$el.html(this.my_template());
         
         var i = 0;
@@ -96,16 +114,27 @@ var EvExercise = Backbone.View.extend({
         $("#babelium-exercise-title").append(exData.response.title);
     },
     
-    record: function ()
+    addTxt: function ()
     {
-        // En la documentacion no viene nada de lo de grabar...
-        $('#exerciseInfo').css('display','none');
-        $('#recordingEndOptions').css('display','inline-block');
+        if($('#txtComCheck').is(':checked'))
+        {
+            $('#txtComDiv').css('display','block');
+        }
+        else
+        {
+            $('#txtComDiv').css('display','none');   
+        }
     },
 
-    discard: function ()
+    addVid: function ()
     {
-        $('#recordingEndOptions').css('display','none');
-        $('#exerciseInfo').css('display','inline-block');
+        if($('#vidComCheck').is(':checked'))
+        {
+            $('#vidComDiv').css('display','block');
+        }
+        else
+        {
+            $('#vidComDiv').css('display','none');   
+        }
     }
 });
