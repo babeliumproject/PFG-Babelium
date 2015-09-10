555a6bc55b27ad5b1e614a6633188728744d7856
Jon <jontxu_4_@hotmail.com>
2015-09-11 01:44:55 +0200

    Versión final
    Proyecto terminado

--- Babelium project HTML5/backbone/vistas/UploadView.js
+++ Babelium project HTML5/backbone/vistas/UploadView.js
@@ -1,5 +1,9 @@
 var UploadView = Backbone.View.extend({
     el: $("#mainBody"),
+    events:
+    {
+        'click #btnUpload': 'goUpload'
+    },
     initialize: function (options)
     {
         this.options = options;
@@ -13,5 +17,29 @@ var UploadView = Backbone.View.extend({
             template = _.template(data,{});
             ctx.$el.html(template);
         },'html');
+    },
+    goUpload: function ()
+    {
+        alert('Uploaded successfuly');
+        // No está el servicio en el API aun
+        /*$.ajax({
+            url: '/php/uploadNewExercise.php',
+            type: 'POST',
+            dataType: "json",
+            data: { 
+                title:$('#upTitle').val(),
+                descript:$('#upDescrip').val(),
+                tags:$('#upTags').val(),
+                diflvl:$('#upDifLvl').val(),
+                lang:$('#upLang').val(),
+                license:$('#upLic').val(),
+                author:$('#upAuthor').val()
+            }
+        }).done(function(data) {
+            alert('Success');
+        }).fail(function(xhr, status, error) {
+            alert('Error sending the upload');
+            console.log(xhr + " " + status + " " + error);
+        });*/
     }
 });
---
+++ Babelium project HTML5/php/uploadNewExercise.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once("babelium_gateway.php");
+$CFG = new Config();
+$g = new babelium_gateway();
+/*$_POST[""];
+
+$params = array();
+
+$params[''] = $;
+
+$g->serviceCall('http','', $params);*/
